59f1cbd1740891a96c74ba9e76daef6ca5080de5
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-04-05 15:21:33 -0500

    refactor skeletal animation

--- ssbh_wgpu/src/animation.rs
+++ ssbh_wgpu/src/animation.rs
@@ -1,9 +1,38 @@
+use std::collections::HashSet;
+
 use ssbh_data::{
     anim_data::{GroupType, TrackValues, Vector3, Vector4},
     prelude::*,
+    skel_data::{BoneData, BoneTransformError},
 };
 
 use crate::{shader::skinning::Transforms, RenderMesh};
+
+// Animation process is Skel, Anim -> Vec<AnimatedBone> -> [Mat4; 512], [Mat4; 512] -> Buffers?
+// Associate an optional transform to override each bone?
+// Evaluate the "tree" of Vec<AnimatedBone> to compute the final world transforms.
+struct AnimatedBone {
+    bone: BoneData,
+    // TODO: Why does decomposing the bone transform itself not work?
+    anim_transform: Option<AnimTransform>,
+    compensate_scale: bool,
+    inherit_scale: bool,
+}
+
+impl AnimatedBone {
+    fn animated_transform(&self) -> glam::Mat4 {
+        self.anim_transform
+            .as_ref()
+            .map(transform_to_mat4)
+            .unwrap_or_else(|| mat4_from_row2d(&self.bone.transform))
+    }
+}
+
+struct AnimTransform {
+    translation: glam::Vec3,
+    rotation: glam::Quat,
+    scale: glam::Vec3,
+}
 
 pub struct AnimationTransforms {
     // Box large arrays to avoid stack overflows in debug mode.
@@ -33,28 +62,45 @@ pub fn apply_animation(
     // TODO: There's probably an efficient execution order using the heirarchy.
     // There might not be enough bones to benefit from parallel execution.
     // We won't worry about redundant matrix multiplications for now.
-    let mut animated_skel = skel.clone();
+    let mut animated_bones: Vec<_> = skel
+        .bones
+        .iter()
+        .map(|b| AnimatedBone {
+            bone: b.clone(),
+            compensate_scale: false,
+            inherit_scale: true,
+            anim_transform: None,
+        })
+        .collect();
+
     if let Some(anim) = anim {
-        animate_skel(anim, &mut animated_skel, frame, meshes);
+        animate_skel(anim, &mut animated_bones, frame, meshes);
     }
+
+    // TODO: Create a second skel clone here?
+    // Apply scale compensation now that parent values are known?
+
+    // TODO: Reimplement calculate_world to take into account scale inheritance.
+    // TODO: Should scale inheritance be part of ssbh_data itself?
+    // TODO: Is there a more efficient way of calculating this?
 
     for (i, bone) in skel.bones.iter().enumerate() {
         // Smash is row-major but glam is column-major.
+        // TODO: Is there an efficient way to calculate world transforms of all bones?
         let bone_world = skel.calculate_world_transform(bone).unwrap();
         let bone_world = glam::Mat4::from_cols_array_2d(&bone_world).transpose();
 
         // TODO: Apply animations?
         // TODO: Separate module with tests to check edge cases.
 
-        let bone_anim_world = animated_skel
-            .calculate_world_transform(
-                animated_skel
-                    .bones
-                    .iter()
-                    .find(|b| b.name == bone.name)
-                    .unwrap(),
-            )
-            .unwrap();
+        let bone_anim_world = calculate_anim_world_transform(
+            &animated_bones,
+            animated_bones
+                .iter()
+                .find(|b| b.bone.name == bone.name)
+                .unwrap(),
+        )
+        .unwrap();
         let bone_anim_world = glam::Mat4::from_cols_array_2d(&bone_anim_world).transpose();
 
         // TODO: Is wgpu expecting row-major?
@@ -73,7 +119,51 @@ pub fn apply_animation(
     }
 }
 
-fn animate_skel(anim: &AnimData, skel: &mut SkelData, frame: f32, meshes: &mut [RenderMesh]) {
+fn mat4_from_row2d(elements: &[[f32; 4]; 4]) -> glam::Mat4 {
+    glam::Mat4::from_cols_array_2d(elements).transpose()
+}
+
+// TODO: Add scale inheritance here?
+fn calculate_anim_world_transform(
+    bones: &[AnimatedBone],
+    bone: &AnimatedBone,
+) -> Result<[[f32; 4]; 4], BoneTransformError> {
+    let mut bone = bone;
+
+    // TODO: Account for inheritance while recursing.
+    let mut transform = bone.animated_transform();
+
+    // Check for cycles by keeping track of previously visited locations.
+    // TODO: Validate the skel once for cycles to make this step faster?
+    let mut visited = HashSet::new();
+
+    // Accumulate transforms by travelling up the bone heirarchy.
+    while let Some(parent_index) = bone.bone.parent_index {
+        if !visited.insert(parent_index) {
+            return Err(BoneTransformError::CycleDetected {
+                index: parent_index,
+            });
+        }
+        if let Some(parent_bone) = bones.get(parent_index) {
+            let parent_transform = parent_bone.animated_transform();
+
+            transform = transform.mul_mat4(&parent_transform);
+            bone = parent_bone;
+        } else {
+            break;
+        }
+    }
+
+    // Save the result in row-major order.
+    Ok(transform.transpose().to_cols_array_2d())
+}
+
+fn animate_skel(
+    anim: &AnimData,
+    bones: &mut [AnimatedBone],
+    frame: f32,
+    meshes: &mut [RenderMesh],
+) {
     for group in &anim.groups {
         match group.group_type {
             GroupType::Transform => {
@@ -82,7 +172,7 @@ fn animate_skel(anim: &AnimData, skel: &mut SkelData, frame: f32, meshes: &mut [
                         if let TrackValues::Transform(values) = &track.values {
                             // TODO: Set the frame/interpolation?
                             // TODO: Faster to use SIMD types?
-                            if let Some(bone) = skel.bones.iter_mut().find(|b| b.name == node.name)
+                            if let Some(bone) = bones.iter_mut().find(|b| b.bone.name == node.name)
                             {
                                 apply_transform_track(frame, track, values, bone);
                             }
@@ -128,30 +218,41 @@ fn interp_vec3(a: &Vector3, b: &Vector3, factor: f32) -> glam::Vec3 {
     glam::Vec3::from(a.to_array()).lerp(glam::Vec3::from(b.to_array()), factor)
 }
 
+fn transform_to_mat4(transform: &AnimTransform) -> glam::Mat4 {
+    let translation = glam::Mat4::from_translation(transform.translation);
+
+    let rotation = glam::Mat4::from_quat(transform.rotation);
+
+    // TODO: Scaling is more complicated and depends on the scale options.
+    let scale = glam::Mat4::from_scale(transform.scale);
+
+    // The application order is scale -> rotation -> translation.
+    // The order is reversed here since glam is column-major.
+    // TODO: Why do we transpose here?
+    (translation * rotation * scale).transpose()
+}
+
 fn apply_transform_track(
     frame: f32,
     track: &ssbh_data::anim_data::TrackData,
     values: &[ssbh_data::anim_data::Transform],
-    bone: &mut ssbh_data::skel_data::BoneData,
+    bone: &mut AnimatedBone,
 ) {
     let (current_frame, next_frame, factor) = frame_values(frame, track);
 
     let current = values[current_frame];
     let next = values[next_frame];
 
-    let translation =
-        glam::Mat4::from_translation(interp_vec3(&current.translation, &next.translation, factor));
-    let rotation = glam::Mat4::from_quat(interp_quat(&current.rotation, &next.rotation, factor));
+    // TODO: Override with rest pose based on transform flags?
+    // TODO: Where to apply transform flags?
+    bone.compensate_scale = track.scale_options.compensate_scale;
+    bone.inherit_scale = track.scale_options.inherit_scale;
 
-    // TODO: Scaling is more complicated and depends on the scale options.
-    let scale = glam::Mat4::from_scale(interp_vec3(&current.scale, &next.scale, factor));
-
-    // The application order is scale -> rotation -> translation.
-    // The order is reversed here since glam is column-major.
-    let anim_transform = translation * rotation * scale;
-
-    // TODO: Why do we not transpose here?
-    bone.transform = anim_transform.to_cols_array_2d();
+    bone.anim_transform = Some(AnimTransform {
+        translation: interp_vec3(&current.translation, &next.translation, factor),
+        rotation: interp_quat(&current.rotation, &next.rotation, factor),
+        scale: interp_vec3(&current.scale, &next.scale, factor),
+    });
 }
 
 fn frame_values(frame: f32, track: &ssbh_data::anim_data::TrackData) -> (usize, usize, f32) {
@@ -170,8 +271,8 @@ fn frame_values(frame: f32, track: &ssbh_data::anim_data::TrackData) -> (usize, 
 mod tests {
     use approx::relative_eq;
     use ssbh_data::{
-        anim_data::{GroupData, NodeData, ScaleOptions, TrackData, Transform},
-        skel_data::BoneData,
+        anim_data::{GroupData, NodeData, ScaleOptions, TrackData, Transform, TransformFlags},
+        skel_data::{BillboardType, BoneData},
     };
 
     use super::*;
@@ -239,6 +340,7 @@ mod tests {
                         [0.0, 0.0, 0.0, 1.0],
                     ],
                     parent_index: None,
+                    billboard_type: BillboardType::Disabled,
                 }],
             },
             Some(&AnimData {
@@ -254,11 +356,10 @@ mod tests {
                             scale_options: ScaleOptions::default(),
                             values: TrackValues::Transform(vec![Transform {
                                 scale: Vector3::new(1.0, 2.0, 3.0),
-                                // TODO: Use a separate Quaternion type?
-                                // Is it worth checking for the zero vector here?
                                 rotation: Vector4::new(1.0, 0.0, 0.0, 0.0),
                                 translation: Vector3::new(4.0, 5.0, 6.0),
                             }]),
+                            transform_flags: TransformFlags::default(),
                         }],
                     }],
                 }],
