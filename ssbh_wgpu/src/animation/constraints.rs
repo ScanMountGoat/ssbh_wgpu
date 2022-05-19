bbf169b6e29e85ad66e4885c076f622f63aa58cb
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-05-18 20:31:28 -0500

    add initial implementation for aim constraints

--- ssbh_wgpu/src/animation/constraints.rs
+++ ssbh_wgpu/src/animation/constraints.rs
@@ -1,5 +1,6 @@
-use super::{AnimTransform, AnimatedBone};
-use ssbh_lib::formats::hlpb::Hlpb;
+use super::{animated_world_transform, AnimTransform, AnimatedBone};
+use glam::Vec4Swizzles;
+use ssbh_lib::formats::hlpb::{AimConstraint, Hlpb};
 
 fn interp(a: f32, b: f32, f: f32) -> f32 {
     (1.0 - f) * a + f * b
@@ -35,6 +36,14 @@ pub fn apply_hlpb_constraints(animated_bones: &mut [AnimatedBone], hlpb: &Hlpb) 
                 std::cmp::Ordering::Less
             });
 
+            // TODO: Allow multiple constraints per bone.
+            // TODO: Can the effects of constraints stack?
+            // TODO: Better handling of application order.
+            // TODO: Also sort these constraints?
+            for aim in &aim_constraints.elements {
+                apply_aim_constraint(animated_bones, aim);
+            }
+
             for orient in orient_constraints_sorted {
                 apply_orient_constraint(animated_bones, orient);
             }
@@ -42,23 +51,75 @@ pub fn apply_hlpb_constraints(animated_bones: &mut [AnimatedBone], hlpb: &Hlpb) 
     }
 }
 
+fn apply_aim_constraint(animated_bones: &mut [AnimatedBone], constraint: &AimConstraint) {
+    // TODO: Investigate the remaining bone name fields.
+    let source = animated_bones
+        .iter()
+        .find(|b| b.bone.name == constraint.aim_bone_name1.to_string_lossy())
+        .cloned()
+        .unwrap();
+
+    // We want the target bone to point at the source bone.
+    // TODO: Is there a way to do this without using the world transforms?
+    let source_world = animated_world_transform(animated_bones, &source)
+        .unwrap()
+        .transpose();
+
+    // TODO: Avoid finding the bone twice?
+    let target_world = animated_world_transform(
+        animated_bones,
+        animated_bones
+            .iter()
+            .find(|b| b.bone.name == constraint.target_bone_name1.to_string_lossy())
+            .unwrap(),
+    )
+    .unwrap()
+    .transpose();
+
+    let target = animated_bones
+        .iter_mut()
+        .find(|b| b.bone.name == constraint.target_bone_name1.to_string_lossy())
+        .unwrap();
+
+    // TODO: Can a bone not affected by the anim be the source?
+    // TODO: Will the target of a constraint ever be animated?
+    let mut target_transform = target
+        .anim_transform
+        .unwrap_or(AnimTransform::from_bone(&target.bone));
+
+    let src_pos = source_world.col(3);
+    let target_pos = target_world.col(3);
+
+    // Get the local axes of the bone to constrain.
+    let aim = (target_world * glam::Vec4::new(1.0, 0.0, 0.0, 0.0)).xyz();
+
+    // Get the vector pointing to the desired bone.
+    let v = src_pos.xyz() - target_pos.xyz();
+
+    // Apply an additional rotation to orient the local axes towards the desired bone.
+    // TODO: How to also incorporate the up vector?
+    target_transform.rotation =
+        target_transform.rotation * glam::Quat::from_rotation_arc(aim.normalize(), v.normalize());
+    target.anim_transform = Some(target_transform);
+}
+
 fn apply_orient_constraint(
     animated_bones: &mut [AnimatedBone],
-    orient: ssbh_lib::formats::hlpb::OrientConstraint,
+    constraint: ssbh_lib::formats::hlpb::OrientConstraint,
 ) {
     // TODO: Investigate the remaining bone name fields.
     let source = animated_bones
         .iter()
-        .find(|b| b.bone.name == orient.parent_bone_name.to_string_lossy())
+        .find(|b| b.bone.name == constraint.parent_bone_name.to_string_lossy())
         .cloned();
 
     if let Some(source) = source {
-        let quat1 = glam::Quat::from_array(orient.quat1.to_array());
-        let quat2 = glam::Quat::from_array(orient.quat2.to_array());
+        let quat1 = glam::Quat::from_array(constraint.quat1.to_array());
+        let quat2 = glam::Quat::from_array(constraint.quat2.to_array());
 
         for target_bone in animated_bones
             .iter_mut()
-            .filter(|b| b.bone.name == orient.driver_bone_name.to_string_lossy())
+            .filter(|b| b.bone.name == constraint.driver_bone_name.to_string_lossy())
         {
             // TODO: Can a bone not affected by the anim be the source?
             // TODO: Will the target of a constraint ever be animated?
@@ -76,12 +137,13 @@ fn apply_orient_constraint(
                 let (source_rot_x, source_rot_y, source_rot_z) =
                     source_transform.rotation.to_euler(glam::EulerRot::XYZ);
 
-                target_transform.rotation = glam::Quat::from_euler(
+                let interp_rotation = glam::Quat::from_euler(
                     glam::EulerRot::XYZ,
-                    interp(target_rot_x, source_rot_x, orient.constraint_axes.x),
-                    interp(target_rot_y, source_rot_y, orient.constraint_axes.y),
-                    interp(target_rot_z, source_rot_z, orient.constraint_axes.z),
+                    interp(target_rot_x, source_rot_x, constraint.constraint_axes.x),
+                    interp(target_rot_y, source_rot_y, constraint.constraint_axes.y),
+                    interp(target_rot_z, source_rot_z, constraint.constraint_axes.z),
                 );
+                target_transform.rotation = interp_rotation;
             }
 
             target_bone.anim_transform = Some(target_transform);
